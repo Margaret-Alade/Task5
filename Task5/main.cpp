da20589938d14b10bd7efc21f5db7b1ffcc84178
Margaret Alade <alade.margaret@yandex.ru>
2024-04-29 13:15:40 +0300

    Correct gitignore 5

--- Task5/main.cpp
+++ Task5/main.cpp
@@ -11,41 +11,55 @@
 class Database {
 public:
     Database() = default;
-    void create_table_person(pqxx::connection& con) {
-        pqxx::work work1(con);
+    void create_table_person(pqxx::work& work1) {
         work1.exec("CREATE TABLE IF NOT EXISTS Person (id serial PRIMARY KEY, name varchar(20) NOT NULL, surname varchar(20) NOT NULL, email varchar(50) NOT NULL);");
         work1.commit();
     }
     
-    void create_table_person_info(pqxx::connection& con) {
-        pqxx::work work1(con);
+    void create_table_person_info(pqxx::work& work1) {
         work1.exec("CREATE TABLE IF NOT EXISTS PersonInfo (id serial PRIMARY KEY, person_id integer NOT NULL REFERENCES Person(id), number varchar(20));");
         work1.commit();
     }
     
-    void insert(pqxx::connection& con, std::string str_query) {
-        pqxx::work work1(con);
+    void insert_person(pqxx::work& work1, std::string str_query) {
         work1.exec(str_query);
         work1.commit();
     }
     
-    void update(pqxx::connection& con, std::string str_query) {
-        pqxx::work work1(con);
+    void insert_num(pqxx::work& work1, std::string str_query) {
         work1.exec(str_query);
         work1.commit();
     }
     
-    void delet(pqxx::connection& con, std::string str_query) {
-        pqxx::work work1(con);
+    void update_person(pqxx::work& work1, std::string str_query) {
         work1.exec(str_query);
         work1.commit();
     }
     
-    void select (pqxx::connection& con, std::string str_query, pqxx::result& rows) {
-        pqxx::work work1(con);
-        rows = work1.exec(str_query);
+    void update_num(pqxx::work& work1, std::string str_query) {
+        work1.exec(str_query);
+        work1.commit();
+    }
+    
+    void delet_person(pqxx::work& work1, std::string str_query) {
+        work1.exec(str_query);
+        work1.commit();
+    }
+    
+    void delet_num(pqxx::work& work1, std::string str_query) {
+        work1.exec(str_query);
+        work1.commit();
+    }
+    
+    void select_person(pqxx::work& work1, pqxx::result& rows) {
+        rows = work1.exec("SELECT id, name, surname, email FROM Person");
+    }
+    
+    void select_person_info(pqxx::work& work1, pqxx::result& rows) {
+        rows = work1.exec("SELECT id, person_id, number FROM PersonInfo WHERE number IS NOT NULL");
     }
 };
+
 
 int main() {
     
@@ -59,38 +73,65 @@ int main() {
     );
         
         std::cout << "Connection is established" << "\n";
-        
         Database database;
+        pqxx::work work1(con);
+        database.create_table_person(work1);
+        pqxx::work work2(con);
+        database.create_table_person_info(work2);
         
-        database.create_table_person(con);
-        database.create_table_person_info(con);
-        database.insert(con, "INSERT INTO Person(name,surname,email) "
-                          "VALUES('Петр', 'Тимофеев', 'petrtimofeev@gmail.com')");
-        database.insert(con, "INSERT INTO Person(name,surname,email) "
-                          "VALUES('Максим', 'Прончев', 'maxpronchev@yandex.ru')");
-        database.insert(con, "INSERT INTO Person(name,surname,email) "
-                          "VALUES('Ольга', 'Сарокина', 'olga.soroka@yandex.ru')");
-        database.update(con,"UPDATE Person SET surname='Сорокина' WHERE surname='Сарокина'");
-
-        database.insert(con, "INSERT INTO PersonInfo(person_id, number) "
-                          "VALUES(1, '+79156094521')");
-        database.insert(con, "INSERT INTO PersonInfo(person_id, number) "
-                          "VALUES(1, '+79995018063')");
-        database.insert(con, "INSERT INTO PersonInfo(person_id) "
-                          "VALUES(2)");
-        database.insert(con, "INSERT INTO PersonInfo(person_id, number) "
-                          "VALUES(3, '+79015320921')");
+        std::string name, surname, email, num;
+        
+        std::cout << "Введите имя 1 клиента:";
+        std::cin >> name;
+        std::cout << "Введите фамилию 1 клиента:";
+        std::cin >> surname;
+        std::cout << "Введите email 1 клиента:";
+        std::cin >> email;
+        pqxx::work work3(con);
+        database.insert_person(work3, "INSERT INTO Person(name, surname, email) "
+                                  "VALUES('" + work3.esc(name) + "', '" + work3.esc(surname) + "', '" + work3.esc(email) + "')");
+        do {
+            std::cout << "Введите номер телефона 1 клиента. Поставьте 0, если не хотите вводить номер. Поставьте 0, если у клиента больше нет номеров:";
+            std::cin >> num;
+            
+            if (num != "0") {
+                pqxx::work work4(con);
+                database.insert_num(work4, "INSERT INTO PersonInfo(person_id, number) "
+                                    "VALUES(1,'" + work4.esc(num) + "')");
+            }
+        } while (num != "0");
+        
+        std::cout << "Введите имя 2 клиента:";
+        std::cin >> name;
+        std::cout << "Введите фамилию 2 клиента:";
+        std::cin >> surname;
+        std::cout << "Введите email 2 клиента:";
+        std::cin >> email;
+        pqxx::work work5(con);
+        database.insert_person(work5, "INSERT INTO Person(name, surname, email) "
+                                  "VALUES('" + work5.esc(name) + "', '" + work5.esc(surname) + "', '" + work5.esc(email) + "')");
+        do {
+            std::cout << "Введите номер телефона 2 клиента. Поставьте 0, если не хотите вводить номер. Поставьте 0, если у клиента больше нет номеров:";
+            std::cin >> num;
+            
+            if (num != "0") {
+                pqxx::work work6(con);
+                database.insert_num(work6, "INSERT INTO PersonInfo(person_id, number) "
+                                    "VALUES(2,'" + work6.esc(num) + "')");
+            }
+        } while (num != "0");
+        
         pqxx::result rows;
-        database.select(con, "SELECT id, name, surname FROM Person", rows);
-        
+        pqxx::work work7(con);
+        database.select_person(work7, rows);
         std::cout << "Список клиентов:" << "\n";
-        
         for (int i = 0; i < rows.size(); i++) {
-            auto[id, name, surname] = rows[i].as<int, std::string, std::string>();
-            std::cout << id << ", " << name << ", " << surname << "\n";
+            auto[id, name, surname, email] = rows[i].as<int, std::string, std::string, std::string>();
+            std::cout << id << ", " << name << ", " << surname <<  ", " << email << "\n";
         }
+        
         pqxx::result rows2;
-        database.select(con, "SELECT id, person_id, number FROM PersonInfo WHERE number IS NOT NULL", rows2);
+        database.select_person_info(work7, rows2);
         
         std::cout << "Список номеров клиентов:" << "\n";
         
@@ -98,7 +139,6 @@ int main() {
             auto[id, person_id, number] = rows2[i].as<int, int, std::string>();
             std::cout << id << ", " << person_id << ", " << number << "\n";
         }
-        
         
     } catch (std::exception& e) {
         std::cout << "Exception: " << e.what() << "\n";
